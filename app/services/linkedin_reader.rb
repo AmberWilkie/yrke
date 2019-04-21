ce7c93697cc3df782dce79aa435011c401aa64ee
amberwilkie <amber@amberwilkie.com>
2019-04-21 09:54:40 +0200

    Add React jobs

--- app/services/linkedin_reader.rb
+++ app/services/linkedin_reader.rb
@@ -2,9 +2,11 @@ module LinkedinReader
   require 'open-uri'
 
   def get_jobs
-    search_url = "https://www.linkedin.com/jobs/search/?distance=5&f_TPR=r604800&keywords=ruby&location=Philadelphia%2C%20Pennsylvania&locationId=PLACES.us.3-3-0-51-1"
-    page = Nokogiri::HTML(open(search_url))
-    jobs = page.css('li.job-result-card')
+    ruby_search = "https://www.linkedin.com/jobs/search/?distance=5&f_TPR=r604800&keywords=ruby&location=Philadelphia%2C%20Pennsylvania&locationId=PLACES.us.3-3-0-51-1&redirect=false&position=1&pageNum=0&f_TP=1"
+    react_search = "https://www.linkedin.com/jobs/search?keywords=React.js&location=Philadelphia%20County%2C%20PA&trk=guest_job_search_jobs-search-bar_search-submit&redirect=false&position=1&pageNum=0&f_TP=1"
+    ruby_page = Nokogiri::HTML(open(ruby_search))
+    react_page = Nokogiri::HTML(open(react_search))
+    jobs = ruby_page.css('li.job-result-card') + react_page.css('li.job-result-card')
 
     # format job listings for adding to spreadsheet
     jobs.map do |job|
